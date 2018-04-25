dcefe63935ccba544d647a5f6fa99014a3eab543
Brett <18648804+BretterJS@users.noreply.github.com>
2018-04-25 11:08:47 -0500

    Update console01.js

--- console01.js
+++ console01.js
@@ -2,6 +2,6 @@
 
 // Usage: console.log('%c <CONSOLE INPUT HERE>', consoleStyle); 
 
-const consoleStyle = ['background: black', 'border: 5px solid #39FF14', 'color: #39FF14', 'display: block'
+let consoleStyle = ['background: black', 'border: 5px solid #39FF14', 'color: #39FF14', 'display: block'
 		, 'text-shadow: 1px 1px green', 'line-height: 40px', 'font-size: 20px', 'text-align: center', 'font-weight: bold', 'margin: 10px'
 		, 'padding: 10px'].join(';');
