ff2b55723e0f4f7d5e24fcb0f96232a51e5e5b5d
Brett <breflip@gmail.com>
2018-04-25 11:05:55 -0500

    Console Style

---
+++ console01.js
@@ -0,0 +1,7 @@
+// Custom styling for the console
+
+// Usage: console.log('%c <CONSOLE INPUT HERE>', consoleStyle); 
+
+const consoleStyle = ['background: black', 'border: 5px solid #39FF14', 'color: #39FF14', 'display: block'
+		, 'text-shadow: 1px 1px green', 'line-height: 40px', 'font-size: 20px', 'text-align: center', 'font-weight: bold', 'margin: 10px'
+		, 'padding: 10px'].join(';');
